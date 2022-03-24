880080f153d13c74fef1e53916c7bfb8baeb532d
raissa <uefsraissa@gmail.com>
2022-03-24 16:09:58 -0300

    operações básicas e atribuição de variáveis

--- JavaScript/.vscode/2-trabalhandoComVariaveis.js
+++ JavaScript/.vscode/2-trabalhandoComVariaveis.js
@@ -3,4 +3,6 @@ console.log("Trabalhando com variáveis");
 
 const idade = 29;
 const nome = "Ricardo";
-let ano = 2020;
+
+// não fazer
+// mes = "Janeiro";
---
+++ JavaScript/.vscode/5-atribuicaoVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 27;
+let nome = "Raissa";
+const sobrenome = "Carneiro";
+
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
