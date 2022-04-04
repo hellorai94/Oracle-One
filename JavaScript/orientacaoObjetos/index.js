b225b1bfeaa8fda4ec3cfff2c38cacc6bdb79871
raissa <uefsraissa@gmail.com>
2022-04-04 20:42:02 -0300

    null, undefined, getters, setters, constructor, static

--- JavaScript/orientacaoObjetos/index.js
+++ JavaScript/orientacaoObjetos/index.js
@@ -1,27 +1,17 @@
 // execução 
 
-import { Cliente} from "./Cliente.js"
+import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+// toda vez que tem um () tem a chamada de um metodo
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo.depositar(500);
+const conta2 = new ContaCorrente(102, cliente2);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
-
-let valor = 400;
+let valor = 200;
 contaCorrenteRicardo.transferir(valor, conta2);
 
-console.log("valor: ", valor)
-console.log(conta2);
-console.log(contaCorrenteRicardo);
+console.log(ContaCorrente.numeroDeContas);
+
