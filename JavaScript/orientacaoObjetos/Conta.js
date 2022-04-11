3e792c88b704eadd54feccfea2a8dd6268ac5b91
raissa <uefsraissa@gmail.com>
2022-04-11 17:35:01 -0300

    aprendendo a usar extends

--- JavaScript/orientacaoObjetos/Conta.js
+++ JavaScript/orientacaoObjetos/Conta.js
@@ -1,56 +1,40 @@
 export class Conta {
-  constructor(tipo, saldoInicial, cliente, agencia){
+  constructor(saldoInicial, cliente, agencia) {
     this._saldo = saldoInicial;
     this._cliente = cliente;
     this._agencia = agencia;
-    this._tipo = tipo;
-}
-
-set cliente(novoValor) {
-  if (novoValor instanceof Cliente){
-    this._cliente = novoValor;
   }
-}
 
-get cliente() {
-  return this._cliente;
-}
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
+    }
+  }
 
-get saldo() {
-  return this._saldo;
-}
+  get cliente() {
+    return this._cliente;
+  }
 
-sacar(valor) {
+  get saldo() {
+    return this._saldo;
+  }
 
-  let taxa = 1
-  if(this._tipo == "corrente"){
-    taxa = 1.1;
-  }
-  if(this._tipo == "salario"){
-    taxa = 1.05;
-  }
-  if(this._tipo == "empresarial"){
-    taxa = 1.15;
-  }
-  const valorSacado = taxa * valor;
-  if (this._saldo >= valor) {
-    this._saldo -= valor;
-    return valor;
-  }
-}
+  sacar(valor) {
 
-depositar(valor) {
-  if (valor <= 100) {
-    return;
+    let taxa = 1
+    const valorSacado = taxa * valor;
+    if (this._saldo >= valorSacado) {
+      this._saldo -= valorSacado;
+      return valorSacado;
+    }
   }
-  this._saldo += valor;
-}
 
-transferir(valor, conta) {
-  if(this._tipo == "salario"){
-    return;
+  depositar(valor) {
+    this._saldo += valor;
   }
-  const valorSacado = this.sacar(valor);
-  conta.depositar(valorSacado);
-}
+
+  transferir(valor, conta) {
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
+  }
 }
--- JavaScript/orientacaoObjetos/index.js
+++ JavaScript/orientacaoObjetos/index.js
@@ -1,17 +1,17 @@
 // execução 
 
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 import {ContaPoupanca} from "./ContaPoupanca.js";
 import {Conta} from "./Conta.js";
 
 // toda vez que tem um () tem a chamada de um metodo
 const cliente1 = new Cliente("Ricardo", 11122233309);
-const contaCorrenteRicardo = new Conta("corrente", 0, cliente1, 1001);
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const contaPoupanca = new Conta("poupanca", 50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
