24fcb45ae1f2e1c7d251c760da96666a3f599e09
raissa <uefsraissa@gmail.com>
2022-04-26 12:52:26 -0300

    finalizei a parte de JavaScript a linguagem da Web

--- JavaScript/orientacaoObjetos/index.js
+++ JavaScript/orientacaoObjetos/index.js
@@ -6,9 +6,15 @@ import {Diretor} from "./Funcionario/Diretor.js";
 import {SistemaAutenticacao} from "./Funcionario/SistemaAutenticacao.js";
 
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
-diretor.cadastrarSenha("123456789")
+diretor.cadastrarSenha("123456")
 const gerente = new Gerente("Ricardo", 5000, 1237899489);
+gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
+const cliente = new Cliente("Lais", 7896145662215, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-console.log(estaLogado);
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
+
+
